1ac3c78b37acb9af643b41ede28539a4ccd72285
JasmineHall1992 <jasminehall5935@gmail.com>
2025-02-12 20:13:19 -0600

    jmh

--- operators.js
+++ operators.js
@@ -2,5 +2,27 @@
  * OPERATORS:
  * 
  * 0: (introduction)
+ * Operators are used to perform different types of mathematical and logical computations.
+ * 
+ * 1: (explanation)
+ * The assignment operator ( = ) assigns value to a variable;
+ * Example:
+ * let x = 10;
+ * 
+ * The addition operator ( + ) add numbers;
+ * Example
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  */
