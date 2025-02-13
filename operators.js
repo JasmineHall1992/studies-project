8e4517f11546bd636526f09145908e5d1e07f402
JasmineHall1992 <jasminehall5935@gmail.com>
2025-02-12 20:30:28 -0600

    jmh

--- operators.js
+++ operators.js
@@ -8,18 +8,29 @@
  *  Types of Operators
  * ------------------------
  * Assignment Operators
+ *     ==> The assignment operator ( = ) assigns a value to a variable.
+ *         Example: 
+ *              let x = z + y;
  * Comparison Operators
- *     ==> Compariaon Operators compare two values (two operands) and evaluate to a Boolean, true or false
- *          Examples:
+ *     ==> Comparison Operators compare two values (two operands) and evaluate to a Boolean, true or false
+ *          Example:
  *                  Binary Comparison Operators
  *                      console.log(1 < 2);
  *                  Strict Comparison Operators 
  *                      console.log(2 === 2);
  * Arithmetic Operators
+ *      ==> Arithmetic Operators are used to perform arithmetic on numbers.
+ *          Example:
+ *              let a = 3;
+ *              let x = (100 + 50) * a; 
  * Logical Operators
  * String Operators
  * Ternary Operators
  * Unary Operators
+ *     ==> Unary Operators is an operation with one operand
+ *          Example: 
+ *                console.log(typeof 1);
+ *                console.lof(!true);
  * 
  * 
  * 
