b5325e68635e55227d7cd39e9894a0eaaa8eaf24
JasmineHall1992 <jasminehall5935@gmail.com>
2025-02-14 16:31:17 -0600

    jmh

--- variables.js
+++ variables.js
@@ -1,6 +1,40 @@
 /**
  * VARIABLES:
  * 
- * 0: (provide general description of the concept)
+ * 0: 
+ *  Variables are containers for values, like a number we might use in a sum, or a string that we might use as part of a sentence.
+ * 
+ * 1: 
+ *  Declaring a variable
+ *      => To use a variable, you've first got to create -- more accurately, we call this declaring the variable. To do this we type the keywords
+ *          const, let or var followed by the name we want to call our variable.
+ *              Example: 
+ *                  let myAge;// undefined variable
+ *         After we declare a variable, we can initialize it with a value, you can do this by typing the variable name, followed by an equals sign
+ *          ( = ), followed by the value you want to give it.
+ *              Example:
+ *                   let myAge = '32';
+ * 2: 
+ *  Reassignment, Hoisting, and Scope
+ *      VAR
+ *          reassignable: yes
+ *          hoisted: yes
+ *          scoped to functions: yes
+ *          scoped to if blocks: no
+ *          scoped to loop blocks: no
+ * 
+ *      LET
+ *          reassignable: yes
+ *          hoisted: no
+ *          scoped to functions: yes
+ *          scoped to if blocks: yes
+ *          scoped to loop blocks: yes
+ * 
+ *      CONST
+ *          reassignable: no
+ *          hoisted: no
+ *          scoped to functions: yes
+ *          scoped to if blocks: yes
+ *          scoped to loop blocks: yes
  * 
  */
