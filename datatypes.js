b67650a20d3e88d9e291df3f46a1fe75229ff1ee
JasmineHall1992 <jasminehall5935@gmail.com>
2025-02-21 16:06:18 -0600

    jmh

--- datatypes.js
+++ datatypes.js
@@ -2,5 +2,19 @@
  * DATATYPES:
  * 
  * 0: (introduction)
+ * Datatypes in JavaScript describe the different types or the kinds of data you will be working with and storing in variables. It is important to learn each of the different data types
+ * because otherwise data can get stored in an improper format which will result in issues in the code later on. There are two categories in which datatypes fall into: primitive, also
+ * known as simple or complex.
  * 
+ * 1: Simple Datatypes
+ * Simple datatpes are atomic and immutable. This means that they do not hold, collect, or aggregate other values. Operations on simple values return new simple values, they do not alter 
+ * the original value. Simple datatypes are also copy by value, which means when assigning or passing, simple data-types are copied from one variable to the next.  There are six different
+ * simple datatypes: Number, String, Boolean, NaN, undefined, and null.
+ *      ===> Examples of each:
+ *          ===> Number: Numeric Data
+ *          ===> String: Character Data
+ *          ===> Boolean: true or false data
+ *          ===> NaN : Not-A-Number
+ *          ===> undefined: no value, not initialized
+ *          ===> null: no value, intentionally nullified by a programmer
  */
